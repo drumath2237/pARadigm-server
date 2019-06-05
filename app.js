f02d4baaa240286141bcd32ed9c1c2d0ef4cda15
drumath <2237k.drumath@gmail.com>
2019-06-05 17:58:21 +0900

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+let http = require('http')
+let ws = require('ws').Server
+let server = http.createServer()
+
+let wss = new ws({server: server})
+let connection = []
+
+wss.on('connection', (socket)=>{
+  console.log('socket connected');
+
+  socket.on('message', (data)=>{
+    console.log('msg: ' + data);
+    wss.clients.forEach((client)=>{
+      client.send(data)
+    })
+  })
+
+  socket.on('close', ()=>{
+
+  })
+})
+
+server.on('request', (req,res)=>{
+  res.writeHead(200, {'Content-Type': 'text/plane'})
+  res.write('hello from node.js')
+  res.end()
+})
+
+server.listen(3000)
+console.log('listen on port 3000');
